a46a407fc5d4bb9b25bc5267ee42972396c57b40
FaberCK <nnnpga@gmail.com>
2026-01-29 19:34:59 +0100

    24hrs clock

--- src/Clock.tsx
+++ src/Clock.tsx
@@ -11,7 +11,7 @@ export function Clock() {
   const timeString = now.toLocaleTimeString([], {
     hour: "2-digit",
     minute: "2-digit",
-    second: "2-digit",
+    hour12: false,
   });
   const dateString = now.toLocaleDateString();
 
